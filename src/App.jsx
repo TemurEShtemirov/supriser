3d5abef7be58988154fbe8d0968418920081ff0c
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-17 08:56:27 +0500

    fix x4 and done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import Flower from './assets/flower.png'
+import Flower from './assets/flower2.png'
 function App() {
   const [showBouquet, setShowBouquet] = useState(false);
 
